c471f53fb81a089d18d281ec4e972fa991dd2dfe
davep80 <davidpaq1@gmail.com>
2023-04-19 00:44:16 -0400

    added use state hook

--- src/App.js
+++ src/App.js
@@ -4,20 +4,37 @@ import Contacts from "./Components/Contacts";
 import NavBar from "./Components/NavBar";
 import Posts from "./Components/Posts";
 import UserProfile from "./Components/UserProfile";
+import { useState } from 'react'
 
 function App() {
+  const [pursuit, setPursuit] = useState(true)
+
+  function handleButtonClick() {
+    setPursuit(!pursuit);
+  }
   return (
     <>
-      <NavBar />
-      <div className="mainArea" style={{ display: 'flex' }}>
-        <div className="left" style={{ minWidth: '75%' }}>
-          <UserProfile />
-          <Posts />
+      <NavBar handleClick={handleButtonClick} />
+      {pursuit && (
+        <div className="mainArea" style={{ display: 'flex' }}>
+          <div className="left" style={{ minWidth: '75%' }}>
+            <UserProfile />
+            <Posts />
+          </div>
+          <div className="right" style={{ minWidth: '25%' }}>
+            <Contacts />
+          </div>
         </div>
-        <div className="right" style={{ minWidth: '25%' }}>
-          <Contacts />
+      )}
+      {!pursuit && (
+        <div className="pursuit" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+          <p>
+            Pursuit is a Full Stack and Computer Programming learning organization
+          </p>
+          <img src="https://i.imgur.com/ULRtXMv.jpg" style={{ margin: '5%' }} alt="pursuit" width="400" height="auto" />
+
         </div>
-      </div>
+      )}
     </>
   )
 };
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function NavBar() {
+function NavBar(props) {
   return (
     <div className='navBar' style={{backgroundColor: 'green', padding: '5px', display: 'flex', justifyContent: 'right'}}>
-      <button>What is Pursuit?</button>
+      <button onClick={props.handleClick}>What is Pursuit?</button>
       <button>Create An Account</button>
       <button>SignIn</button>
     </div>
