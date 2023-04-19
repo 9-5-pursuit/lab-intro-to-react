2d589c9fde636bb93e4b4805bbef63dd54fa665c
Onyx McQueen <onyxmcqueen88@gmail.com>
2023-04-19 01:05:23 -0400

    Completed Lab

---
+++ src/Components/ContactUserCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ContactUserCard() {
+    return (
+        <div className='usercard'>
+        <h3>Jordan Walke</h3>
+        <h4>React Creator</h4>
+        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="post" width="200" height="200" />
+        <p>Lorem Ipsum</p>
+        </div>  
+    );
+}
+
+export default ContactUserCard;
---
+++ src/Components/UserProfile.js
@@ -0,0 +1,14 @@
+import REACT from 'react';
+
+function UserProfile() {
+    return (
+        <div className='User'>
+        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="post" width="200" height="200" />
+        <h2>Jordan Walke</h2>
+        <h3>React creator</h3>
+        <p>Lorem Ipsum</p>
+        </div>
+    )
+}
+
+export default UserProfile;
