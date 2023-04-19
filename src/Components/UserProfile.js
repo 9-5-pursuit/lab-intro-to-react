624abc4d088bd24d4a852a73c2c67101f70bf7d6
Devin Lewis <devinlewis@pursuit.org>
2023-04-19 01:59:48 -0400

    Styled and Passes test

---
+++ src/Components/UserProfile.js
@@ -0,0 +1,17 @@
+function UserProfile() {
+    return (
+        <>
+            <div className="userprofile">
+                <aside>
+                    <img src="profile.png" alt="Profile" />
+                </aside>
+                <main>
+                    <h2>Jordan Walke</h2>
+                    <h3>React Creator</h3>
+                    <p>Lorem ipsem</p>
+                </main>
+            </div>
+        </>
+    );
+}
+export default UserProfile;
