939086b0f1c62934b0cc7a647229b3eab456f9f4
jeremytjimenez <jeremytjimenez@gmail.com>
2023-04-18 21:14:00 -0400

    removed code

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import "./index.css";
 import NavBar from './Components/NavBar';
 import UserProfile from './Components/UserProfile';
 import Posts from './Components/Posts';
-import Post from './Components/Post';
 import Contacts from './Components/Contacts';
-import ContactUserCard from './Components/ContactUserCard'
 
 function App () {
   return (
