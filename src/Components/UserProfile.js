2fc0383e640cfeb8a01f345855df3b2994ffe002
Fabio Tavarez <fabiotavarez@pursuit.org>
2023-04-19 06:18:48 -0400

    Lab completed

---
+++ src/Components/UserProfile.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "../profile.png";
+
+function UserProfile() {
+  return (
+    <div>
+      <img src={Image} alt="" width="100" height="200" />
+      <h2>Jordan Walke</h2>
+      <p>React Creator</p>
+      <p>Lorem Ipsem</p>
+    </div>
+  );
+}
+
+export default UserProfile;
