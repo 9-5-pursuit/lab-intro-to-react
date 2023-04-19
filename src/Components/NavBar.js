ce0623cad8b460654259bb6bc31d4669a12b77dc
lgxxhi <luisgsilva95@gmail.com>
2023-04-19 00:39:11 -0400

    Sorta Complete, I think?

---
+++ src/Components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <>
+      <div className="navBar">
+        <button type="button">What is Pursuit?</button>
+        <button type="button"> Create an Account</button>
+        <button type="button">Sign In</button>
+      </div>
+    </>
+  );
+}
+
+export default NavBar;
---
+++ src/Components/UserProfile.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function UserProfile() {
+  return (
+    <div className="userProfile">
+      <img src="profile.png" />
+      <h4>Jordan Walke</h4>
+      <h5>React Creator</h5>
+      <p>lorem ipsem</p>
+    </div>
+  );
+}
+
+export default UserProfile;
