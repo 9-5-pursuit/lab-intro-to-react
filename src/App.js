a806d3770652d73796bc07a33670bd03a83a28fa
Irfan Hanjra <ihanjra@gmail.com>
2023-04-19 02:54:24 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React from 'react';
 import "./index.css";
+import NavBar from './Components/NavBar';
+import UserProfile from "./Components/UserProfile";
+import Posts from "./Components/Posts";
+import Post from "./Components/Post";
+import Contacts from "./Components/Contacts";
+import ContactUserCard from "./Components/ContactUserCard";
 
 function App () {
-  return <p>Hello, world!</p>;
+  return (
+    <div className="App">
+      
+      <div style={{ backgroundColor: "lightblue" }}>
+        <NavBar />
+      </div>
+
+      <div>
+        <UserProfile />
+      </div>
+
+      <div>
+        <Posts />
+         <Post />
+         <Post />
+        < Post />
+        <div>
+      
+      <div>
+        <Contacts />
+          <ContactUserCard />
+          <ContactUserCard />
+          <ContactUserCard />
+          <ContactUserCard />
+      </div>
+    </div>
+   </div>
+  </div>
+  );
 };
 
 export default App;
---
+++ src/Components/ContactUserCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ContactUserCard() {
+  return (
+    <>
+      <img src="./profile.png" alt="User Profile" />
+      <h1>Jordan Walke</h1>
+      <h3>React Creator</h3>
+      <p>Lorem Ipsum</p>
+    </>
+  );
+}
+
+export default ContactUserCard;
