ce0c18aef79a645330448ff6482f6ec261774c73
Naomi-Pate <patenaomi99@gmail.com>
2023-04-19 01:43:03 -0400

    css completed

--- src/Components/ContactUserCard.js
+++ src/Components/ContactUserCard.js
@@ -5,7 +5,7 @@ function ContactUserCard() {
     <div class="ContactUserCard">
     <h3>Jordan Walke</h3>
     <h4>React Creator</h4>
-    <img src="./public/profile.png" alt="post" width="200" height="200" />
+    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="post" width="200" height="200" />
     <p>Lorem Ipsem</p>
     </div>
   );
--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ContactUserCard from './ContactUserCard';
 
 function UserProfile() {
   return (
     <div class="UserProfile">
-    <ContactUserCard />
+    <h3>Jordan Walke</h3>
+    <h4>React Creator</h4>
+    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="post" width="200" height="200" />
+    <p>Lorem Ipsem</p>
     </div>
   );
 }
