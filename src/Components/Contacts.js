10d29ad88e43bd7683190d22cc028cdc8dc5ea7a
davep80 <davidpaq1@gmail.com>
2023-04-18 23:58:07 -0400

    formatting

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -2,19 +2,21 @@ import React from 'react'
 import ContactUserCard from './ContactUserCard'
 
 function Contacts() {
-  return (
-    <>
-    <div className='Contacts' style={{border: '1px solid black', padding: '10px', textAlign: 'center', justifyContent: 'center', 
-    display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '3%'}}>
-        <h3>Contacts</h3>
-        <ContactUserCard/>
-        <ContactUserCard/>
-        <ContactUserCard/>
-        <ContactUserCard/>
-    </div>
-    </>
-    
-  )
+    return (
+        <>
+            <div className='Contacts' style={{
+                border: '1px solid black', padding: '10px', textAlign: 'center', justifyContent: 'center',
+                display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '3%'
+            }}>
+                <h3>Contacts</h3>
+                <ContactUserCard />
+                <ContactUserCard />
+                <ContactUserCard />
+                <ContactUserCard />
+            </div>
+        </>
+
+    )
 }
 
 export default Contacts
--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -2,15 +2,17 @@ import React from 'react'
 import Post from './Post'
 
 function Posts() {
-  return (
-    <div className='Posts' style={{border: '1px solid black', padding: '10px', textAlign: 'center', justifyContent: 'center', 
-    display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '3%'}}>
-        <h3>Posts</h3>
-        <Post/>
-        <Post/>
-        <Post/>
+    return (
+        <div className='Posts' style={{
+            border: '1px solid black', padding: '10px', textAlign: 'center', justifyContent: 'center',
+            display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '3%'
+        }}>
+            <h3>Posts</h3>
+            <Post />
+            <Post />
+            <Post />
         </div>
-  )
+    )
 }
 
 export default Posts
