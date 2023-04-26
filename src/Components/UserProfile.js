3b4ec3da215b317f5b2c5ee213c31d4a8533afe3
Blui322 <luistejada@pursuit.com>
2023-04-26 18:00:26 -0400

    done

---
+++ src/Components/UserProfile.js
@@ -0,0 +1,14 @@
+const UserProfile = () => {
+    return (
+        <div className="user-profile">
+            <img src="./profile.png"></img>
+            <div>
+              <h2>Jordan Water</h2>
+              <h3>React Creator</h3>
+              <p>Lorem Ipsum</p>
+            </div>
+        </div>
+    );
+};
+
+export default UserProfile;
